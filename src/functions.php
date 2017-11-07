e28deb5e4cc3aa216c39eb516135cf115d5b095f
Steve Yorke <steve.yorke@gmail.com>
2017-11-07 10:36:26 -0800

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -5,6 +5,4 @@ add_theme_support( 'post-thumbnails' );
 // Register CustomPostTypes via Bundler file
 require_once(get_template_directory().'/wordpress/cpts/bundle.cpt.php');
 
-// Register built-in plugins via add_plugins file
-require_once(get_template_directory().'/wordpress/plugins/add_plugins.php');
 ?>
